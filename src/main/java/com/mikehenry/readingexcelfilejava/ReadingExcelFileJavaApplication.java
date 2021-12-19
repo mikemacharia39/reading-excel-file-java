c4d547ef877e099fca1c9d1e9f5fb4002077ca14
mikemacharia39 <mikemacharia39@gmail.com>
2021-12-19 16:58:19 +0300

    upload starter

---
+++ src/main/java/com/mikehenry/readingexcelfilejava/ReadingExcelFileJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mikehenry.readingexcelfilejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingExcelFileJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingExcelFileJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikehenry/readingexcelfilejava/ReadingExcelFileJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikehenry.readingexcelfilejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingExcelFileJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
